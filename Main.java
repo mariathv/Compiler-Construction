1f26ddd39ba06d6ae4ccc6a0be7f4341cb886875
Maria Naeem Khokhar <114730306+mariathv@users.noreply.github.com>
2025-02-17 22:43:17 +0500

    error handler added

--- Main.java
+++ Main.java
@@ -1,36 +1,37 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         LexicalAnalyzer lexer = new LexicalAnalyzer();
-        
+
         String inputProgram = """
                 intgr x = 10;
                 chr y = 'A';
                 true;
                 false;
-                my_var = 25;
+                0my_var = 25;
                 """;
-        
-//        String inputProgram = """
-//                intgr x = 10;
-//                chr y = 'A';
-//                boo b = true;
-//                dcl z = 2.1;
-//        		dcl e = 3.11115;
-//        		intgr e = 2^2;
-//                true;
-//                false;
-//                my_var = 25;
-//                inp(num);
-//                out("yaya");
-//                out(numm);
-//                string = "i want to kill myself"
-//                ## dont do this
-//                ## kys please
-//        		   dont do this yes hahaj
-//                """;
-        
+
+        // String inputProgram = """
+        // intgr x = 10;
+        // chr y = 'A';
+        // boo b = true;
+        // dcl z = 2.1;
+        // dcl e = 3.11115;
+        // intgr e = 2^2;
+        // true;
+        // false;
+        // my_var = 25;
+        // inp(num);
+        // out("yaya");
+        // out(numm);
+        // string = "i want to kill myself"
+        // ## dont do this
+        // ## kys please
+        // dont do this yes hahaj
+        // """;
+
         System.out.println("Tokenized Output:");
         List<String> tokens = lexer.tokenize(inputProgram.replaceAll("[;]", " "));
         for (String token : tokens) {
---
+++ Utilities.java
@@ -0,0 +1,17 @@
+
+public class Utilities {
+	public  final String RESET = "\u001B[0m";
+    public  final String BLACK = "\u001B[30m";
+    public  final String RED = "\u001B[31m";
+    public  final String GREEN = "\u001B[32m";
+    public  final String YELLOW = "\u001B[33m";
+    public  final String BLUE = "\u001B[34m";
+    public  final String PURPLE = "\u001B[35m";
+    public  final String CYAN = "\u001B[36m";
+    public  final String WHITE = "\u001B[37m";
+    public  final String YELLOW_BG = "\033[43m";
+    
+    Utilities() {
+    	
+    }
+}
